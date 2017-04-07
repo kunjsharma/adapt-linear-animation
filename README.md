d20f7dd8d0fbb5b9e1f326a0b8e745c4e94b17de
Kunj <kunjsharma@hotmail.com>
2017-04-07 12:28:00 +0530

    Component initiated.
    Added component from local GitHub.

---
+++ README.md
@@ -0,0 +1,46 @@
+# adapt-linear-animation  
+
+**linear-animation** is a *linear animation component* developed by Kunj B Sharma <kunjsharma@hotmail.com> https://github.com/kunjsharma/adapt-linear-animation.git.  
+
+<img src="https://github.com/adaptlearning/documentation/blob/master/04_wiki_assets/plug-ins/images/text01.png" alt="sample text component">
+
+**linear-animation**'s simple purpose is to present text/images in linear animation form. The text may include HTML.
+
+### Attributes
+
+[**core model attributes**](https://github.com/adaptlearning/adapt_framework/wiki/Core-model-attributes): These are inherited by every Adapt component. [Read more](https://github.com/adaptlearning/adapt_framework/wiki/Core-model-attributes).
+
+**_component** (string): This value must be: `text`.
+
+**_classes** (string): CSS class name to be applied to **Text**’s containing `div`. The class must be predefined in one of the Less files. Separate multiple classes with a space.
+
+**_layout** (string): This defines the horizontal position of the component in the block. Acceptable values are `full`, `left` or `right`.  
+
+**title** (string): A reference title for the component. **title** is distinct from the **displayTitle** which, if present, appears above the component. **title** provides the opportunity to use a shortened form in tighter spaces, such as in menus or in the drawer.  
+
+**displayTitle** (string): Optional text that will display as a title or header above the component. It can be used as a headline.   
+
+**_animation** (string): Whether animation required or not.
+
+**_delay** (string): Time interval between display of 2 images in miliseconds.
+
+**_bg** (string): Background image refrence. v0.0.1 requires it to display component correctly in responsive.
+
+**_items** (array): Each *item* represents one choice for the animation frames and contains values for **text** and **_graphic**
+
+**instruction** (string): This optional text appears above the component. It is frequently used to
+guide the learner’s interaction with the component.
+
+**body** (string): Although optional, this text constitutes what is thought of as the primary *text* of the **Text** component. HTML is permitted.  
+<div float align=right><a href="#top">Back to Top</a></div>
+
+## Limitations
+
+To be confirmed. 
+
+
+----------------------------
+**Version number:**  0.0.1
+**Framework versions:** 2.0.15  
+**Author / maintainer:** Kunj B Sharma <kunjsharma@hotmail.com>   
+**Accessibility support:** WAI AA 
---
+++ js/adapt-linear-animation.js
@@ -0,0 +1,68 @@
+/*
+ * adapt-linear-animation
+ * License - 
+ * Maintainers - Kunj B Sharma <kunjsharma@hotmail.com>
+ */
+define(function (require) {
+    var ComponentView = require('coreViews/componentView');
+    var Adapt = require('coreJS/adapt');
+    var Handlebars = require('handlebars');
+    var LinearAnimation = ComponentView.extend({
+        preRender: function () {
+            //OUR HBS template requires this helper
+            if (Handlebars.helpers && !('ifvalue' in Handlebars.helpers)) {
+                Handlebars.registerHelper('ifvalue', function (conditional, options) {
+                    if (options.hash.value === conditional) {
+                        return options.fn(this)
+                    } else {
+                        return options.inverse(this);
+                    }
+                });
+            }
+        },
+        postRender: function () {
+            this.setReadyStatus();
+            if (this.model.get('_animation') == "false") {
+                this.$(".linear-animation-item-frame").css({
+                    "opacity": 1
+                });
+                this.setCompletionStatus();
+                return;
+            }
+            var scolevel0 = this;
+            var delay = Number(this.model.get('_delay'));
+            var isie8 = $('html').hasClass('ie8') || $('html').hasClass('ie7');
+            this.$(".linear-animation-widget").bind('inview', function (event, visible) {
+                if (visible == true) {
+                    if (isie8) {
+                        scolevel0.$(".linear-animation-item-frame img").css({
+                            "opacity": "1",
+                            "position": "absolute"
+                        });
+                        scolevel0.setCompletionStatus();
+                        return;
+                    } else {
+                        var $item = $(this).find('.linear-animation-item');
+                        //var $item = $('.linear-animation-item');
+                        var slideCount = $item.length;
+                        $item.each(function (index) {
+                            var scoLevel1 = this;
+                            $(this).children(".linear-animation-item-text").delay(delay * index).animate({
+                                opacity: 1
+                            }, 500, function () {
+                                $(scoLevel1).children(".linear-animation-item-frame").delay(50).animate({
+                                    opacity: 1
+                                }, 500, function () {
+                                    if (index == (slideCount - 1)) {
+                                        scolevel0.setCompletionStatus();
+                                    }
+                                });
+                            });
+                        });
+                    }
+                }
+            });
+        }
+    });
+    Adapt.register("linear-animation", LinearAnimation);
+});
